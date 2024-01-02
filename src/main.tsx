9b0721841b3e9af096ef46489b1a61d5b7644e78
YunisAslan <yunisaslanov397@gmail.com>
2024-01-02 11:27:50 +0400

    delete: redux stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
 import { AuthContextProvider } from "./services/context/AuthContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </Provider>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
